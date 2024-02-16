3220fd9fc65a42e65c9eb83f4f48f8ec0b267fce
chobits tai <chobits@itri.org.tw>
2024-02-16 15:53:51 +0800

    reduce ros buf

--- vins_estimator/src/rosNodeTest.cpp
+++ vins_estimator/src/rosNodeTest.cpp
@@ -242,8 +242,8 @@ int main(int argc, char **argv)
 
     registerPub(n);
 
-    ros::Subscriber sub_imu = n.subscribe(IMU_TOPIC, 2000, imu_callback, ros::TransportHints().tcpNoDelay());
-    ros::Subscriber sub_feature = n.subscribe("/feature_tracker/feature", 2000, feature_callback);
+    ros::Subscriber sub_imu = n.subscribe(IMU_TOPIC, 100, imu_callback, ros::TransportHints().tcpNoDelay());
+    ros::Subscriber sub_feature = n.subscribe("/feature_tracker/feature", 10, feature_callback);
 #if 0
     ros::Subscriber sub_img0 = n.subscribe(IMAGE0_TOPIC, 100, img0_callback);
     ros::Subscriber sub_img1 = n.subscribe(IMAGE1_TOPIC, 100, img1_callback);
